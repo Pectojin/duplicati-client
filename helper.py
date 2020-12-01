f4d6e1a185d206bae986f531fa06385b2652a5ee
Rune Henriksen <contact@henriksen.dk>
2020-12-02 00:05:41 +0100

    stop making timezone translations in format_time

--- helper.py
+++ helper.py
@@ -29,16 +29,6 @@ def format_time(time_string, precise=False):
     # Now for comparison
     now = datetime.datetime.now()
 
-    try:
-        # Take care of timezones
-        now = now.replace(tzinfo=tz.tzutc())
-        now = now.astimezone(tz.tzlocal())
-        datetime_object = datetime_object.replace(tzinfo=tz.tzutc())
-        datetime_object = datetime_object.astimezone(tz.tzlocal())
-    except Exception as exc:
-        common.log_output(exc, False)
-        return None
-
     # Get the delta
     if datetime_object > now:
         delta = (datetime_object - now)
